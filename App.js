c30e0c0dd85e2e477eca4aed6b6b49d1b6abc77f
Johnny Kreitzman <kreitzka@gmail.com>
2019-11-11 11:13:54 -0600

    chat app created, messages not sending/receiving at this time

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Store from './Store';
+
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Store>
+        <Dashboard />
+      </Store>
+    </div>
+  );
+}
+
+export default App;
---
+++ Store.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import io from 'socket.io-client'
+
+export const CTX = React.createContext();
+
+const initState = {
+    general: [
+        {from: 'aaron', msg: 'hello'},
+        {from: 'aaron', msg: 'hello'},
+        {from: 'aaron', msg: 'hello'}
+    ],
+    topic2: [
+        {from: 'johnny', msg: 'hello'},
+        {from: 'thomas', msg: 'hello'},
+        {from: 'daniel', msg: 'hello'}
+    ]
+}
+
+
+function reducer(state, action) {
+    const {from, msg, topic} = action.payload;
+    switch(action.type){
+        case 'RECEIVE_MESSAGE':
+            return {
+                ...state,
+                [topic]: [
+                    ...state[topic],
+                    {from, msg}   
+                ]
+            }
+        default:
+            return state
+    }
+}
+
+let socket;
+
+function sendChatAction(value) {
+    socket.emit('chat message', value);
+}
+
+export default function Store(props){
+
+    const [allChats, dispatch] = React.useReducer(reducer, initState)
+
+
+    if(!socket) {
+        socket=io(':3001');
+        socket.on('chat message', function(msg){
+            dispatch({type:'RECEIVE_MESSAGE', payload:msg});
+        });
+    }
+
+    const user = 'johnny' + Math.random(100).toFixed(2)
+        
+    return (
+        <CTX.Provider value={{allChats, sendChatAction, user}} >
+            {props.children}
+        </CTX.Provider>
+    )
+}
